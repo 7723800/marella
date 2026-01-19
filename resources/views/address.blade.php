89d0a8ee87a9fc8f71cd5a0a8e03c90a3f7fd891
sergeytalko <7723800@mail.ru>
2026-01-19 16:24:42 +0500

    some refactoring

--- resources/views/address.blade.php
+++ resources/views/address.blade.php
@@ -5,7 +5,7 @@
         <div class="container">
             <div class="address-wrapper">
                 <p>
-                <div class="address-title">Магазин в Астане:</div>
+                <div class="address-title">Магазины в Астане:</div>
                 </p>
                 <p>
                 <div class="address-text">ТРЦ "Керуен", Достык 9 </div>
@@ -13,26 +13,26 @@
                 <p><a href="tel:+77770015373"></a>+7 777 001 53 73</p>
                 <p>Часы работы: ПН-ВС: 10.00 – 22.00 без перерывов и выходных</p>
                 </br>
-                {{-- <div class="address-text">•ул. Мангилик Ел, 53</div>
+                <p>
+                <div class="address-text">ул. Мангилик Ел, 53</div>
                 </p>
                 <p><a href="tel:+77172440404"></a>+7 7172 44 04 04</p>
                 <p><a href="tel:+77770015373"></a>+7 777 001 53 73</p>
                 <p>Часы работы: ПН-ВС: 10.00 – 20.00 без перерывов и выходных</p>
                 </br>
                 <p>
-                <div class="address-text">•ул. Сарайшык, 34/2</div>
+                <div class="address-text">ул. Сарайшык, 34/2</div>
                 </p>
                 <p><a href="tel:+77172989900"></a>+7 7172 98 99 00</p>
                 <p><a href="tel:+77770015373"></a>+7 777 001 53 73</p>
                 <p>Часы работы: ПН-ВС: 10.00 – 20.00 без перерывов и выходных</p>
                 </br>
                 <p>
-                <div class="address-text">•ул Агыбай Батыра 1</div>
+                <div class="address-text">ул Агыбай Батыра 1</div>
                 </p>
                 <p><a href="tel:+77172322189"></a>+7 7172 32 21 89</p>
                 <p><a href="tel:+77770015373"></a>+7 777 001 53 73</p>
                 <p>Часы работы: ПН-ВС: 11.00 – 20.00 без перерывов и выходных</p>
-                <y-map></y-map> --}}
             </div>
         </div>
     </div>
